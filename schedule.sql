b042cc2c4e06f35903f11a613b75ba7cbed81fb5
anyubinn <anyubin0724@naver.com>
2025-03-19 19:16:53 +0900

    fix: schedule.sql 수정

--- schedule.sql
+++ schedule.sql
@@ -5,6 +5,7 @@ CREATE TABLE calendar
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     todo VARCHAR(100) NOT NULL,
     writer_name VARCHAR(50) NOT NULL,
+    password BIGINT NOT NULL,
     reg_date DATE,
     mod_date DATE
 );
