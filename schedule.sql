120eb8c6dff84f744c4542f0d4879a76ee50b9a6
anyubinn <anyubin0724@naver.com>
2025-03-19 15:34:55 +0900

    feat: schedule.sql을 통해 calendar 테이블 생성

---
+++ schedule.sql
@@ -0,0 +1,10 @@
+use calendar;
+
+CREATE TABLE calendar
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    todo VARCHAR(100) NOT NULL,
+    writer_name VARCHAR(50) NOT NULL,
+    reg_date DATE,
+    mod_date DATE
+);
