2c763f939365195751b3bbefd996020418fe8322
anyubinn <anyubin0724@naver.com>
2025-03-24 14:28:55 +0900

    feat: writer 테이블 생성 및 calendar 테이블에 외래키 추가

--- schedule.sql
+++ schedule.sql
@@ -11,3 +11,19 @@ CREATE TABLE calendar
 );
 
 ALTER TABLE calendar MODIFY COLUMN password VARCHAR(50) NOT NULL;
+
+ALTER TABLE calendar DROP COLUMN writer_name;
+
+ALTER TABLE calendar DROP COLUMN password;
+
+CREATE TABLE writer
+(
+    writer_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    writer_name VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL
+);
+
+ALTER TABLE calendar ADD COLUMN writer_id BIGINT;
+
+ALTER TABLE calendar ADD CONSTRAINT fk_writer FOREIGN KEY (writer_id) REFERENCES writer(writer_id);
